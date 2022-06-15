1be7af0bba0695e0bd1801414839c19872455349
jeanjik <45577016+jeanjik@users.noreply.github.com>
2022-06-15 16:14:57 +0200

    added setter for newClassExpression

--- src/main/java/NodeInfo.java
+++ src/main/java/NodeInfo.java
@@ -1,6 +1,7 @@
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLObjectUnionOf;
@@ -11,6 +12,7 @@ import java.util.stream.Stream;
 
 @Builder
 @Getter
+@Setter
 @AllArgsConstructor
 public class NodeInfo implements RdfSerializable{
 
@@ -18,9 +20,8 @@ public class NodeInfo implements RdfSerializable{
     private final TableauxIndividual individual;
     private final Stream<OWLClassExpression> classExpressions;
     private final Set<OWLObjectUnionOf> alreadyVisitedUnions;
-    private final OWLClassExpression newClassExpression;
+    private OWLClassExpression newClassExpression;
     private final OWLObjectPropertyAssertionAxiom propertyAssertionAxiom;
-    private final boolean checkLazyUnfoldingRule;
 
     public static NodeInfo getNewNode(NodeInfo oldNode, Stream<OWLClassExpression> newClassExpressions,
                                       OWLClassExpression newClassExpression, OWLObjectUnionOf visitingUnionOf){
@@ -32,7 +33,6 @@ public class NodeInfo implements RdfSerializable{
                 .individual(oldNode.getIndividual().clone())
                 .classExpressions(newClassExpressions)
                 .newClassExpression(newClassExpression)
-                .checkLazyUnfoldingRule(true)
                 .build();
     }
 
