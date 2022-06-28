8ef28914ff1350ab7fcb8f0170992b5b494a87c9
jeanjik <45577016+jeanjik@users.noreply.github.com>
2022-06-28 17:34:20 +0200

    added logging for hermit

--- src/test/java/LoadAndSaveOntologyTest.java
+++ src/test/java/LoadAndSaveOntologyTest.java
@@ -3,94 +3,93 @@ import intelligent.web.alc.ALCReasoner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.semanticweb.HermiT.ReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.io.FileDocumentSource;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
 import java.io.File;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class LoadAndSaveOntologyTest {
 
     private OWLOntologyManager manager;
     private OWLOntology ontology;
-    private OWLDataFactory dataFactory;
     private OWLReasonerFactory reasonerFactory;
     private OWLReasoner standardReasoner;
+    private final Logger LOGGER = getLogger(this.getClass());
 
     @BeforeEach
     void setUp() {
         this.manager = OWLManager.createOWLOntologyManager();
-        this.dataFactory = this.manager.getOWLDataFactory();
         this.reasonerFactory = new ReasonerFactory();
     }
-    @Test
+    @Test //NON SODDISFACIBILE
     void ontology3_0Test() throws OWLOntologyCreationException {
         this.ontology = loadFromFile(this.manager, "ontology3_0.txt");
         this.standardReasoner = reasonerFactory.createReasoner(this.ontology);
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("B and (R only owl:Nothing)");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
-    @Test
+    @Test //NON SODDISFACIBILE
     void ontology3_1Test() throws OWLOntologyCreationException {
         this.ontology = loadFromFile(this.manager, "ontology3_1.txt");
         this.standardReasoner = reasonerFactory.createReasoner(this.ontology);
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("B and (R only owl:Nothing)");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
+
     }
 
-    @Test
+    @Test //BLOCKING E SODDISFACIBILE
     void ontology1Test1() throws OWLOntologyCreationException {
         this.ontology = loadFromFile(this.manager, "ontology1.txt");
         this.standardReasoner = reasonerFactory.createReasoner(this.ontology);
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("(A1 or A2) and ((R only (A1 and A2)) or (not A1)) and (B1 or B2) and ((R some (not A1)) or (R some (not A2)))");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
-    @Test
+    @Test //SODDISFACIBILE
     void ontology1Test2() throws OWLOntologyCreationException {
         this.ontology = loadFromFile(this.manager, "ontology1.txt");
         this.standardReasoner = reasonerFactory.createReasoner(this.ontology);
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("((R some (A1 and A2)) or (R some (not B1))) and (B1 or B2) and (B3 or B4) and ((R only (not A1)) or (R only (not A2)))");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
-    @Test
+    @Test //SODDISFACIBILE
     void ontology2Test1() throws OWLOntologyCreationException {
         this.ontology = loadFromFile(this.manager, "ontology2.txt");
         this.standardReasoner = reasonerFactory.createReasoner(this.ontology);
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("((S some (R only A)) or B) and ((S some C) or D) and (not D or not B) and ((S only (R some (not A))) or (S only (not C)))");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
-    @Test
+    @Test //SODDISFACIBILE
     void ontology2Test2() throws OWLOntologyCreationException {
         this.ontology = loadFromFile(this.manager, "ontology2.txt");
         this.standardReasoner = reasonerFactory.createReasoner(this.ontology);
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("(A or B) and ((S only (R some (not A))) or (S only B)) and (not B or (R only A)) and (not A or (S some (R only A)))");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
     @Test
@@ -100,8 +99,7 @@ public class LoadAndSaveOntologyTest {
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("((R only A) or (R only (not A))) and (A or B) and ((R only (S only A)) or (not B)) and (R some (not A))");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
     @Test
@@ -111,8 +109,7 @@ public class LoadAndSaveOntologyTest {
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("(A or B) and (C or D) and ((R only (S only A)) or (not B)) and (R some (S only (not A)))");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
     @Test
@@ -122,8 +119,7 @@ public class LoadAndSaveOntologyTest {
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("D");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
     @Test
@@ -133,8 +129,7 @@ public class LoadAndSaveOntologyTest {
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("not D");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
     @Test
@@ -144,8 +139,7 @@ public class LoadAndSaveOntologyTest {
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("(not (P some A)) and (not (P only (not A)))");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
     @Test
@@ -155,8 +149,7 @@ public class LoadAndSaveOntologyTest {
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("B and (R only owl:Nothing)");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
     }
 
     @Test
@@ -166,8 +159,26 @@ public class LoadAndSaveOntologyTest {
         ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
         OWLClassExpression cl = alcQueryParser.parseClassExpression("VeggiePizza and MeatPizza");
         ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
-        alcReasoner.flush();
-        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), standardReasoner.isSatisfiable(cl));
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
+    }
+
+    @Test //APPLICAZIONE LAZY UNFOLDING A FIGLIO
+    void pizzaTest2() throws OWLOntologyCreationException {
+        this.ontology = loadFromFile(this.manager, "pizza.txt");
+        this.standardReasoner = reasonerFactory.createReasoner(this.ontology);
+        ALCQueryParser alcQueryParser = new ALCQueryParser(this.ontology);
+        OWLClassExpression cl = alcQueryParser.parseClassExpression("hasTopping some Veggie");
+        ALCReasoner alcReasoner = new ALCReasoner(this.ontology);
+        calculateHermitTimeAndAssertEquals(cl, alcReasoner);
+    }
+
+    private void calculateHermitTimeAndAssertEquals(OWLClassExpression cl, ALCReasoner alcReasoner) {
+        long hermitStartTime = System.nanoTime();
+        boolean satisfiableHermit = standardReasoner.isSatisfiable(cl);
+        long hermitEndTime = System.nanoTime();
+        long totalHermitTime = (hermitEndTime - hermitStartTime)/1000000;
+        LOGGER.info("Hermit execution time: " + totalHermitTime + "ms");
+        Assertions.assertEquals(alcReasoner.isSatisfiable(cl), satisfiableHermit);
     }
 
     private OWLOntology loadFromFile(@Nonnull OWLOntologyManager manager, String fileName) throws OWLOntologyCreationException {
